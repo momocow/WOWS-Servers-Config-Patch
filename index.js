6f74892fc13a7431e1404d8ac6719d8aed6179ef
MomoCow <momocow.me@gmail.com>
2017-10-15 22:12:55 +0800

    WoWS directory detection not yet

--- index.js
+++ index.js
@@ -5,14 +5,19 @@ if(process.platform == 'win32'){
 
   const lnkDirCandidates = ['WoWS Weekend', 'World of Warships']
 
+  console.log('Auto-detecting WoWS root directory from start menu')
   for(let lnkDir of lnkDirCandidates){
-    let lnkPath = _path.join(process.env.APPDATA, 'Microsoft', 'Windows', 'Start Menu', 'Programs', lnkDir)
-
+    let lnkPath = _path.join(
+      process.env.APPDATA, 'Microsoft', 'Windows', 'Start Menu',
+      'Programs', lnkDir, 'World of Warships.lnk')
+      
+    console.log(`Trying ${lnkPath}`)
     // file guard
     try{
       fs.accessSync(lnkPath, fs.R_OK)
     }
     catch(err){
+      console.log('The provided symbolic link is not found')
       continue
     }
 
