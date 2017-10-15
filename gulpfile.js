21538a59be1c442fdc0da1f04d57120e0f43e2cf
MomoCow <momocow.me@gmail.com>
2017-10-15 22:12:35 +0800

    Making release with prebuilt node executable

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,20 @@
+const _fs = require('fs-extra')
 const _gulp = require('gulp')
 const _nexe = require('nexe')
 
-_gulp.task('release', ''
-)
-
-
-_nexe.compile({
-  input: './index.js',
-  output: './dist/WoWS-servers-patch.exe'
-}).then(() => {
-  console.log('success')
+_gulp.task('release', function(){
+  process.env.PROJECT_ROOT = './'
+  _fs.ensureDirSync('./dist')
+  return _nexe.compile({
+    input: './index.js',
+    output: './dist/WoWS-servers-patch.exe',
+    build: true,
+    target: 'windows-x64-8.5.0',
+    ico: './assets/icon/wows.ico',
+    loglevel: 'verbose'
+  }).then(() => {
+    console.log('A release is built for Windows-x64')
+  })
 })
+
+_gulp.task('default', ['release'])
